f879ea984603442257932b02504c6cd2ba45bd90
Jay <113779115+jayminnie@users.noreply.github.com>
2026-05-13 04:37:20 +0000

    modify nav

--- NolMart-master/js/main.js
+++ NolMart-master/js/main.js
@@ -59,10 +59,13 @@ function adjustBodyPadding() {
 
 function addNavBoxShadow(mainSubContainer, navSubContainer) {
     if (window.scrollY > 0) {mainSubContainer.classList.add('active')} 
-    else {mainSubContainer.classList.remove('active')}
+    else {
+        mainSubContainer.classList.remove('active');
+        navSubContainer.classList.remove('active')
+    }
 
-    if (window.scrollY > 30) {navSubContainer.classList.add('active')} 
-    else {navSubContainer.classList.remove('active');}
+    if (window.scrollY > 35) {navSubContainer.classList.add('active')} 
+    else {navSubContainer.classList.remove('active')}
 }
 
 // Execute all setup functions when the DOM is fully loaded
