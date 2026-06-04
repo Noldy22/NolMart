e18798e0195c0a249fb4a5d33637ffa094a397f3
JanicesTry <113779115+JanicesTry@users.noreply.github.com>
2026-06-04 14:03:29 +0400

    Test Images

--- scripts/build-products.js
+++ scripts/build-products.js
@@ -7,8 +7,8 @@ const OUTPUT_FILE = path.join(__dirname, '../public/products.json');
 
 // UPDATE THIS: Replace 'YOUR_USERNAME' and 'YOUR_REPO_NAME' with your actual GitHub details
 //const GITHUB_BASE_URL = 'https://raw.githubusercontent.com/jayminnie/nolmart/main/public';
-const GITHUB_BASE_URL = '/public';
-//const GITHUB_BASE_URL = 'https://raw.githubusercontent.com/Noldy22/NolMart/master/public';
+//const GITHUB_BASE_URL = '/public';
+const GITHUB_BASE_URL = 'https://raw.githubusercontent.com/Noldy22/NolMart/master/public';
 
 function buildProducts() {
   try {
