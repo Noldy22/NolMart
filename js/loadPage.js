230082daa19758a20fcb5c1aa1a8dd7a749f3c92
JanicesTry <113779115+JanicesTry@users.noreply.github.com>
2026-06-24 02:12:27 +0400

    Update Guides

--- js/loadPage.js
+++ js/loadPage.js
@@ -4,15 +4,15 @@ const mainSection = document.querySelector('main');
 const noContentMessage = document.querySelector('.no-content-message');
 
 export function showPageAfterLoad() {
-    scrollToTop();
-
     if (mainSection) mainSection.style.display = 'block';
     if (noContentMessage) noContentMessage.style.display = 'none';
+
+    scrollToTop();
 }
 
 export function hidePageDuringLoad() {
-    scrollToTop();
-
     if (mainSection) mainSection.style.display = 'none';
     if (noContentMessage) noContentMessage.style.display = 'flex';
+
+    scrollToTop();
 }
