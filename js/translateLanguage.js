b5cae8f323788676b501534d72716f9011cfd06e
JanicesTry <113779115+JanicesTry@users.noreply.github.com>
2026-06-24 01:57:46 +0400

    test

--- js/translateLanguage.js
+++ js/translateLanguage.js
@@ -21,8 +21,9 @@ async function translateCache(cache, text, lang1, lang2) {
     const textLength = text.length;
     let translated;
 
+    console.log(text);
+
     if (textLength > 499) {
-      console.log(text);
       const firstHalf = text.slice(0,Math.round(textLength/2));
       const secondHalf = text.slice(Math.round(textLength/2), textLength);
 
