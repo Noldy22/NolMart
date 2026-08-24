f79d446a2038d22a6866225b44304ed750c1b885
jayminnie <janiceminjaofficial@gmail.com>
2026-08-25 02:38:14 +0400

    Update

--- admin/cms.js
+++ admin/cms.js
@@ -1,15 +1,12 @@
 CMS.registerEventListener({
-    name: 'preSave',
-    handler: ({ entry }) => {
-  
-      // Check which collection this entry belongs to
-      if (entry.get('collection') !== 'guides') {
-        return entry;
-      }
-  
-      // Modify the actual content data
-      return entry
-        .get('data')
-        .set('updatedAt', new Date().toISOString());
-    },
+  name: 'preSave',
+  handler: ({ entry }) => {
+    if (entry.get('collection') !== 'guides') {
+      return entry;
+    }
+
+    return entry
+      .get('data')
+      .set('updatedAt', new Date().toISOString());
+  },
 });
